333ed536abb42370beaba45a66cb9337cecc8119
ilovecwq <2625338464@qq.com>
2022-09-20 20:57:56 +0800

    5-3 使用ref和reactive实现数据绑定

---
+++ web/src/views/Home.vue
@@ -0,0 +1,88 @@
+<template>
+  <a-layout>
+    <a-layout-sider width="200" style="background: #fff">
+      <a-menu
+          mode="inline"
+          :style="{ height: '100%', borderRight: 0 }"
+      >
+        <a-sub-menu key="sub1">
+          <template #title>
+              <span>
+                <user-outlined />
+                subnav 11111
+              </span>
+          </template>
+          <a-menu-item key="1">option1</a-menu-item>
+          <a-menu-item key="2">option2</a-menu-item>
+          <a-menu-item key="3">option3</a-menu-item>
+          <a-menu-item key="4">option4</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template #title>
+              <span>
+                <laptop-outlined />
+                subnav 2
+              </span>
+          </template>
+          <a-menu-item key="5">option5</a-menu-item>
+          <a-menu-item key="6">option6</a-menu-item>
+          <a-menu-item key="7">option7</a-menu-item>
+          <a-menu-item key="8">option8</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub3">
+          <template #title>
+              <span>
+                <notification-outlined />
+                subnav 3
+              </span>
+          </template>
+          <a-menu-item key="9">option9</a-menu-item>
+          <a-menu-item key="10">option10</a-menu-item>
+          <a-menu-item key="11">option11</a-menu-item>
+          <a-menu-item key="12">option12</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <pre>
+        {{ ebooks }}
+        {{ ebooks2 }}
+      </pre>
+
+    </a-layout-content>
+  </a-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent,onMounted,ref,reactive,toRef } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import axios from 'axios'
+export default defineComponent({
+  name: 'Home',
+//  通过axios调用电子书列表接口
+//  初始化的逻辑都写到onMounted方法里，setup就放一些参数定义/方法定义
+//  多利用生命周期函数
+  setup(){
+    console.log("setup");
+    const ebooks = ref();
+    const ebooks1 = reactive({books: []});
+    onMounted(() => {
+      console.log("onMounted");
+      axios.get("http://localhost:8880/ebook/list?name=Spring").then((response) => {
+        const data = response.data;
+        ebooks.value = data.content;
+        ebooks1.books = data.content;
+        console.log(response);
+      });
+    });
+    return{
+      ebooks,
+      ebooks2:toRef(ebooks1,"books")
+    }
+  }
+  //新增的初始化方法
+
+});
+</script>
