7b3d477fea15996f2dbc1d9b38f7318f9228622c
ilovecwq <2625338464@qq.com>
2022-09-22 18:27:17 +0800

    5-5 修改axios的baseUrl都配置文件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -98,8 +98,8 @@ export default defineComponent({
     const ebooks = ref();
     const ebooks1 = reactive({books: []});
     onMounted(() => {
-      console.log("onMounted111");
-      axios.get(process.env.VUE_APP_SERVER + "/ebook/list").then((response) => {
+      console.log("onMounted222");
+      axios.get("/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content.list;
         ebooks1.books = data.content;
