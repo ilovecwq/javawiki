6c1387eb63992bc9605a270179d2242ef1dac317
ilovecwq <2625338464@qq.com>
2022-09-28 17:13:50 +0800

    6-10 增加删除电子书功能

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -19,14 +19,47 @@
               <a-button type="primary" @click="edit(record)">
                 编辑
               </a-button>
-              <a-button type="danger">
-                删除
-              </a-button>
+              <a-popconfirm
+                  title="删除后不可恢复，确认删除?"
+                  ok-text="是"
+                  cancel-text="否"
+                  @confirm="handleDelete(record.id)"
+              >
+                <a-button type="danger">
+                  删除
+                </a-button>
+              </a-popconfirm>
             </a-space>
           </template>
         </a-table>
       </a-layout-content>
     </a-layout>
+
+    <a-modal
+        title="电子书表单"
+        v-model:visible="modalVisible"
+        :confirm-loading="modalLoading"
+        @ok="handleModalOk"
+    >
+      <a-form :model="ebook" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+        <a-form-item label="封面">
+          <a-input v-model:value="ebook.cover" />
+        </a-form-item>
+        <a-form-item label="名称">
+          <a-input v-model:value="ebook.name" />
+        </a-form-item>
+        <a-form-item label="分类">
+          <a-cascader
+              v-model:value="categoryIds"
+              :field-names="{ label: 'name', value: 'id', children: 'children' }"
+              :options="level1"
+          />
+        </a-form-item>
+        <a-form-item label="描述">
+          <a-input v-model:value="ebook.description" type="textarea" />
+        </a-form-item>
+      </a-form>
+    </a-modal>
 </template>
 
 
@@ -137,34 +170,39 @@ export default defineComponent({
         size: pagination.value.pageSize
       });
     });
-    return {
-      ebooks,
-      pagination,
-      columns,
-      loading,
-      handleTableChange
-    }
-  }
-});
-//
-//     // -------- 表单 ---------
-//     /**
-//      * 数组，[100, 101]对应：前端开发 / Vue
-//      */
-//     const categoryIds = ref();
-//     const ebook = ref();
-//     const modalVisible = ref(false);
-//     const modalLoading = ref(false);
-//     const handleModalOk = () => {
-//       modalLoading.value = true;
-//       ebook.value.category1Id = categoryIds.value[0];
-//       ebook.value.category2Id = categoryIds.value[1];
-//       axios.post("/ebook/save", ebook.value).then((response) => {
-//         modalLoading.value = false;
-//         const data = response.data; // data = commonResp
-//         if (data.success) {
-//           modalVisible.value = false;
-//
+
+
+
+    // -------- 表单 ---------
+    /**
+     * 数组，[100, 101]对应：前端开发 / Vue
+     */
+    // const categoryIds = ref();
+    const ebook = ref();
+    const modalVisible = ref(false);
+    const modalLoading = ref(false);
+    const handleModalOk = () => {
+      modalLoading.value = true;
+      // setTimeout(() => {
+      //
+      // }, 2000);
+      axios.post("/ebook/save", ebook.value).then((response) => {
+        const data = response.data; // data = commonResp
+        if(data.success){
+          modalLoading.value = false;
+          modalVisible.value = false;
+          // 重新加载列表
+          handleQuery({
+            page: pagination.value.current,
+            size: pagination.value.pageSize
+          });
+        }
+      });
+    };
+      // ebook.value.category1Id = categoryIds.value[0];
+      // ebook.value.category2Id = categoryIds.value[1];
+
+
 //           // 重新加载列表
 //           handleQuery({
 //             page: pagination.value.current,
@@ -176,37 +214,42 @@ export default defineComponent({
 //       });
 //     };
 //
-//     /**
-//      * 编辑
-//      */
-//     const edit = (record: any) => {
-//       modalVisible.value = true;
-//       ebook.value = Tool.copy(record);
-//       categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id]
-//     };
+    /**
+     * 编辑
+     */
+    const edit = (record: any) => {
+      modalVisible.value = true;
+      ebook.value = record;
+      // categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id]
+    };
+
+    /**
+     * 新增
+     */
+    const add = () => {
+      modalVisible.value = true;
+      ebook.value = {};
+    };
+    const handleDelete = (id: number) => {
+      axios.delete("/ebook/delete/" + id).then((response) => {
+        const data = response.data; // data = commonResp
+        if (data.success) {
+          // 重新加载列表
+          handleQuery({
+            page: pagination.value.current,
+            size: pagination.value.pageSize,
+          });
+        }
+      });
+    };
+    onMounted(() => {
+      handleQuery({
+        page: 1,
+        size: pagination.value.pageSize
+      });
+    });
 //
-//     /**
-//      * 新增
-//      */
-//     const add = () => {
-//       modalVisible.value = true;
-//       ebook.value = {};
-//     };
-//
-//     const handleDelete = (id: number) => {
-//       axios.delete("/ebook/delete/" + id).then((response) => {
-//         const data = response.data; // data = commonResp
-//         if (data.success) {
-//           // 重新加载列表
-//           handleQuery({
-//             page: pagination.value.current,
-//             size: pagination.value.pageSize,
-//           });
-//         } else {
-//           message.error(data.message);
-//         }
-//       });
-//     };
+
 //
 //     const level1 =  ref();
 //     let categorys: any;
@@ -252,29 +295,46 @@ export default defineComponent({
 //     onMounted(() => {
 //       handleQueryCategory();
 //     });
-//
+// //
 //     return {
-//       param,
+//       // param,
 //       ebooks,
 //       pagination,
 //       columns,
 //       loading,
 //       handleTableChange,
-//       handleQuery,
-//       getCategoryName,
+//       // handleQuery,
+//       // getCategoryName,
 //
 //       edit,
-//       add,
+//       // add,
 //
-//       ebook,
+//       // ebook,
 //       modalVisible,
 //       modalLoading,
 //       handleModalOk,
-//       categoryIds,
-//       level1,
-//
-//       handleDelete
+//       // categoryIds,
+//       // level1,
+//       //
+//       // handleDelete
 //     }
 //   }
 // });
+    return {
+      ebooks,
+      pagination,
+      columns,
+      loading,
+      handleTableChange,
+      edit,
+      add,
+      ebook,
+      modalVisible,
+      modalLoading,
+      handleModalOk,
+      handleDelete,
+
+    }
+  }
+});
 </script>
